0a3e81ec99519be93d4151eb6df59b8e3773adda
Daniel Lucena <daniel.lucena@olxbr.com>
2022-10-08 05:40:15 -0300

    add DetailsUser

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
+
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { CreateUserController } from "./controllers/user/CreateUserController";
+import { DetailsUserController } from "./controllers/user/DetailsUserController";
 
 const router = Router();
 
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
+router.get('/me', new DetailsUserController().handle)
 
 export { router };
