d6a3100cdaa67e523bb3df30e90beafdb5a9ba00
Daniel Lucena <daniel.lucena@olxbr.com>
2022-10-08 08:20:21 -0300

    create category

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,10 +5,14 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { DetailsUserController } from "./controllers/user/DetailsUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+
 const router = Router();
 
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 router.get('/me', isAuthenticated, new DetailsUserController().handle)
+
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
 export { router };
