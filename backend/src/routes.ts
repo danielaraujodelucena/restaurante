033b3ea418e5855265f99876e2e639a5d282dfc2
Daniel Lucena <daniel.lucena@olxbr.com>
2022-10-08 06:05:52 -0300

    middlerawe isAuthenticated

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,11 +3,12 @@ import { Router } from "express";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { DetailsUserController } from "./controllers/user/DetailsUserController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
 
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
-router.get('/me', new DetailsUserController().handle)
+router.get('/me', isAuthenticated, new DetailsUserController().handle)
 
 export { router };
