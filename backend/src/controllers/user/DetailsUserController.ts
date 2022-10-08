659aa4b55cf4a991f657a37680c983d90b755e80
Daniel Lucena <daniel.lucena@olxbr.com>
2022-10-08 06:32:51 -0300

    show details user

--- backend/src/controllers/user/DetailsUserController.ts
+++ backend/src/controllers/user/DetailsUserController.ts
@@ -3,9 +3,11 @@ import { DetailsUserService } from "../../services/user/DetailsUserService";
 
 class DetailsUserController {
     async handle(req: Request, resp: Response) {
+        const user_id = req.user_id;
+
         const detailsUserService = new DetailsUserService();
 
-        const user = await detailsUserService.execute();
+        const user = await detailsUserService.execute(user_id);
 
         return resp.json(user);
     }
