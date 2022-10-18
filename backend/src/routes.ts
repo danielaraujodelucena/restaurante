433481d388d6eb35e573ea63f344d1073caff1e7
Daniel Lucena <daniel.lucena@olxbr.com>
2022-10-18 11:49:14 -0300

    finish order

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -18,6 +18,7 @@ import { RemoveItemController } from "./controllers/order/RemoveItemController";
 import { SendOrderController } from "./controllers/order/SendOrderController";
 import { ListOrdersController } from "./controllers/order/ListOrdersController";
 import { DetailOrderController } from "./controllers/order/DetailOrderCOntroller";
+import { FinishOrderController } from "./controllers/order/FinishOrderController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import uploadConfig from './config/multer';
@@ -42,5 +43,6 @@ router.delete('/order/remove', isAuthenticated, new RemoveItemController().handl
 router.put('/order/send', isAuthenticated, new SendOrderController().handle)
 router.get('/orders', isAuthenticated, new ListOrdersController().handle)
 router.get('/order/detail', isAuthenticated, new DetailOrderController().handle)
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle)
 
 export { router };
