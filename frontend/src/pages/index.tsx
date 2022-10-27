083439c8cb6cd92ac85a9cde7b575beeb1ae920a
Daniel Lucena <daniel.lucena@olxbr.com>
2022-10-27 11:15:30 -0300

    adjusting index page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,9 @@
 import Head from "next/head"
+import Image from "next/image"
+import styles from '../../styles/home.module.scss'
+
+import logoRestaurante from '../../public/logo-restaurante.png'
+import { Input } from "../components/ui/Input"
 
 export default function Home() {
   return (
@@ -6,8 +11,15 @@ export default function Home() {
       <Head>
         <title>Seja bem-vindo</title>
       </Head>
-      <div>
-        <h1>Home</h1>
+      <div className={styles.containerCenter}>
+        <Image src={logoRestaurante} alt="Logo do restaurante" width={150} />
+      
+        <div className={styles.login}>
+          <form>
+            <Input placeholder="Email" type="text" />
+            <Input placeholder="Senha" type="password" />
+          </form>
+        </div>
       </div>
     </>
   )
