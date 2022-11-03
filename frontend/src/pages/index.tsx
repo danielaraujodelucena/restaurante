60a74175325bce04311cdab21689b69991135f3c
Daniel Lucena <daniel.lucena@olxbr.com>
2022-11-03 08:24:50 -0300

    create AuthContext

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useContext, FormEvent, useState } from "react"
 import Head from "next/head"
 import Image from "next/image"
 import styles from '../../styles/home.module.scss'
@@ -5,10 +6,26 @@ import styles from '../../styles/home.module.scss'
 import logoRestaurante from '../../public/logo-restaurante.png'
 import { Input } from "../components/ui/Input"
 import { Button } from "../components/ui/Button"
-
+import { AuthContext } from "../contexts/AuthContext"
 import Link from "next/link"
 
 export default function Home() {
+  const { signIn } = useContext(AuthContext);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  async function handleLogin(event: FormEvent){
+    event.preventDefault();
+    let data = {
+      email,
+      password
+    }
+    
+    await signIn(data);
+  }
+
   return (
     <>
       <Head>
@@ -18,10 +35,26 @@ export default function Home() {
         <Image src={logoRestaurante} alt="Logo do restaurante" width={150} />
       
         <div className={styles.login}>
-          <form>
-            <Input placeholder="Email" type="text" />
-            <Input placeholder="Senha" type="password" />
-            <Button type="submit" loading={false}> Acessar </Button>
+          <form onSubmit={handleLogin}>
+            <Input 
+              placeholder="Email" 
+              type="text" 
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)} 
+            />
+            
+            <Input 
+              placeholder="Senha" 
+              type="password" 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+            />
+            
+            <Button 
+              type="submit" 
+              loading={false}> 
+                Acessar 
+            </Button>
           </form>
 
           <Link href="/signup" className={styles.text}>
