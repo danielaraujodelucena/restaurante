849da0647092697c961dd8788c5af10bb9499a7a
Daniel Lucena <daniel.lucena@olxbr.com>
2022-11-08 08:54:36 -0300

    signUp user

--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,3 +1,4 @@
+import { useState, FormEvent, useContext } from "react"
 import Head from "next/head"
 import Image from "next/image"
 import styles from '../../../styles/home.module.scss'
@@ -6,9 +7,40 @@ import logoRestaurante from '../../../public/logo-restaurante.png'
 import { Input } from "../../components/ui/Input"
 import { Button } from "../../components/ui/Button"
 
+import { AuthContext } from "../../contexts/AuthContext"
+
 import Link from "next/link"
 
 export default function SignUp() {
+  const { signUp } = useContext(AuthContext);
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [loading, setLoading] = useState(false);
+
+  async function handleSignUp(event:FormEvent) {
+    event.preventDefault();
+
+    if(name === ''|| email === '' || password === '') {
+      alert("Preencha todos os campos");
+      return;
+    }
+
+    setLoading(true);
+
+    let data = {
+      name,
+      email,
+      password
+    }
+
+    await signUp(data);
+
+    setLoading(false);
+  }
+
   return (
     <>
       <Head>
@@ -20,11 +52,29 @@ export default function SignUp() {
         <div className={styles.login}>
           <h1>Criando sua conta</h1>
           
-          <form>
-            <Input placeholder="Nome" type="text" />
-            <Input placeholder="Email" type="text" />
-            <Input placeholder="Senha" type="password" />
-            <Button type="submit" loading={false}> Cadastrar </Button>
+          <form onSubmit={handleSignUp}>
+            <Input 
+              placeholder="Nome" 
+              type="text"
+              value={name}
+              onChange={ (e) => setName(e.target.value) } 
+            />
+
+            <Input 
+              placeholder="Email" 
+              type="text"
+              value={email}
+              onChange={ (e) => setEmail(e.target.value) } 
+            />
+
+            <Input 
+              placeholder="Senha" 
+              type="password"
+              value={password}
+              onChange={ (e) => setPassword(e.target.value) } 
+            />
+
+            <Button type="submit" loading={loading}> Cadastrar </Button>
           </form>
 
           <Link href="/" className={styles.text}>
